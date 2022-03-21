96b20e937ce6fe22d32c4f189bf7ff24ff4f1817
kmcclenn <kmcclennen@jhcschool.org>
2022-03-20 19:53:46 -0600

    fixed secret key security

--- senior_project/api/models.py
+++ senior_project/api/models.py
@@ -1,11 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from address.models import AddressField
 
-class User(models.User):
+class User(AbstractUser):
     pass
 
 # Create your models here.
 class Restaurant(models.Model):
     name = models.CharField(max_length=64)
+    address = AddressField(on_delete = models.CASCADE, default = None)
 
     #def __str__(self):
         #return self.name
--- senior_project/senior_project/settings.py
+++ senior_project/senior_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w05*49w8x@f1wqkzd)d_!us90@4id0y&*@t8)5z55be^wpr!8x'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,8 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
-    'rest_framework'
+    'rest_framework',
+    'address'
 ]
+AUTH_USER_MODEL = "api.User"
+
+GOOGLE_API_KEY = 'AIzaSyBQRs6DeE6XY1kUvlfAMgn0-QLD9n0Zf3Q'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
