a80b83a98c14814ac1794b0aee14d306b451cf1b
kmcclenn <kmcclennen@jhcschool.org>
2022-05-18 23:14:40 -0600

    fixed typo

--- api/serializer.py
+++ api/serializer.py
@@ -74,7 +74,7 @@ def return_inputted_waittime(validated_data):
     if validated_data['wait_length'] is None:
         wait_length = (validated_data['seated_time'] - validated_data['arrival_time']).total_seconds() / 60
     else:
-        wait_length - validated_data['wait_length']
+        wait_length = validated_data['wait_length']
 
     inputted_waittime = InputtedWaittime(restaurant=validated_data['restaurant'],
                     wait_length=wait_length,
