84fae87b94db105d7c7106c1d5067cfe9b76a130
kmcclenn <kmcclennen@jhcschool.org>
2022-05-20 10:18:54 -0600

    working on custom address permission

--- api/utils.py
+++ api/utils.py
@@ -27,13 +27,13 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
 
         return obj.id == request.user.id ## if both are none, then it works too - so works when creating new user.
 
-class MustBeAdminToChange(permissions.IsAuthenticated):
+class MustBeAdminToChange(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
         if request.method == "PUT" and request.user.is_staff:
             return True
-        return super().has_object_permission(self, request, view, obj)
+        return request.user.is_authenticated
 
 def get_credibility(user_id):
     reporting_user = AppUser.objects.get(id = user_id)
